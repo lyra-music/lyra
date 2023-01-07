625a22c4b4b7c94afd0c76a4791856fa3c95af24
Fridenity <43757589+Fridenity@users.noreply.github.com>
2023-01-08 02:21:08 +0700

    More rust & optimisations + More organised tests

--- lyra/python/__main__.py
+++ lyra/python/__main__.py
@@ -7,10 +7,14 @@ dotenv.load_dotenv('../.env')  # pyright: ignore [reportUnknownMemberType]
 from src import bot, activity
 
 
-if __name__ == '__main__':
+def main() -> None:
     if os.name != 'nt':
         import uvloop
 
         uvloop.install()
 
     bot.run(activity=activity)
+
+
+if __name__ == '__main__':
+    main()
