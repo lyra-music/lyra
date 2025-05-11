17c61fa357e1b2de89922938cbef509d1349ec2b
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2025-05-11 17:29:53 +0700

    Make /config now-playing immediately take effect If there exists an existing current now-playing message (msg send/delete)

--- lyra/src/component/config/now_playing.rs
+++ lyra/src/component/config/now_playing.rs
@@ -2,10 +2,12 @@ use lyra_ext::num::u64_to_i64_truncating;
 use twilight_interactions::command::{CommandModel, CreateCommand};
 
 use crate::{
+    LavalinkAware,
     command::{SlashCtx, macros::out, model::BotSlashCommand, require},
-    core::model::DatabaseAware,
+    core::model::{DatabaseAware, OwnedHttpAware},
     error::CommandResult,
     gateway::GuildIdAware,
+    lavalink::{DelegateMethods, NowPlayingData},
 };
 use lyra_proc::BotCommandGroup;
 
@@ -32,9 +34,27 @@ impl BotSlashCommand for Toggle {
         .await?
         .now_playing;
 
+        let maybe_data = require::player(&ctx).map(|p| p.data());
         let (emoji, action) = if new_now_playing {
+            if let Ok(data) = maybe_data {
+                let data_r = data.read().await;
+                if let Ok(track) = require::current_track(data_r.queue()) {
+                    let (c_data, gid) = (ctx.lavalink().data(), ctx.guild_id().into());
+                    let np_data = NowPlayingData::new(&c_data, gid, &data_r, track.track).await?;
+                    drop(data_r);
+                    data.write()
+                        .await
+                        .new_now_playing_message_in(ctx.http_owned(), np_data, ctx.channel_id())
+                        .await?;
+                }
+            }
             ("🔔", "Sending")
         } else {
+            if let Ok(data) = maybe_data {
+                if data.read().await.now_playing_message_id().is_some() {
+                    data.write().await.delete_now_playing_message(&ctx).await;
+                }
+            }
             ("🔕", "Not sending")
         };
 
