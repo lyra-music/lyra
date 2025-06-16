d8ec5441b7132cfc8e3667b7594bab0fe0150141
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2025-06-16 17:40:49 +0700

    refactor: simplify box dyn error in lyra/build.rs
    Simplify the `Box<dyn Error>` in `lyra/build.rs` to `impl Debug`

--- lyra/build.rs
+++ lyra/build.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 fn main() {
     println!("cargo:rerun-if-changed=../migrations");
     println!("cargo:rerun-if-changed=../preset");
@@ -6,12 +8,12 @@ fn main() {
     }
 }
 
-fn emit() -> Result<(), Box<dyn std::error::Error>> {
-    Ok(vergen_git2::Emitter::default()
+fn emit() -> Result<(), impl Debug> {
+    vergen_git2::Emitter::default()
         .add_instructions(&vergen_git2::BuildBuilder::all_build()?)?
         .add_instructions(&vergen_git2::CargoBuilder::all_cargo()?)?
         .add_instructions(&vergen_git2::Git2Builder::all_git()?)?
         .add_instructions(&vergen_git2::RustcBuilder::all_rustc()?)?
         .add_instructions(&vergen_git2::SysinfoBuilder::all_sysinfo()?)?
-        .emit()?)
+        .emit()
 }
